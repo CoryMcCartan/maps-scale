fa82ef90d6efab2ee78ef881ac33c9c87c648bb9
Cory McCartan <cory.mccartan@gmail.com>
2016-12-14 23:31:59 -0600

    minor updates

--- js/main.js
+++ js/main.js
@@ -4,17 +4,20 @@ let leftLevel = 0, rightLevel = 0;
 function init() {
     let options = {
         zoom: 9,
-        mapTypeControl: false,
         streetViewControl: false,
     };
 
     let leftOptions = {
         zoomControl: false,
+        mapTypeControlOptions: {
+            position: google.maps.ControlPosition.LEFT_BOTTOM,
+        },
         center: {lng: -122.23, lat: 47.52},
     };
     Object.assign(leftOptions, options);
 
     let rightOptions = {
+        mapTypeControl: false,
         center: {lng: -122.24, lat: 37.64},
     };
     Object.assign(rightOptions, options);
@@ -22,6 +25,7 @@ function init() {
     lMap = new google.maps.Map($("#left"),  leftOptions);
     rMap = new google.maps.Map($("#right"), rightOptions);
 
+    lMap.addListener("maptypeid_changed", handleMapType);
     lMap.addListener("zoom_changed", handleLeftZoom);
     rMap.addListener("zoom_changed", handleRightZoom);
 
@@ -29,7 +33,7 @@ function init() {
     let rBox = new google.maps.places.SearchBox($("#rightSearch"));
 
     lMap.controls[google.maps.ControlPosition.TOP_LEFT].push($("#leftSearch"));
-    rMap.controls[google.maps.ControlPosition.TOP_LEFT].push($("#rightSearch"));
+    rMap.controls[google.maps.ControlPosition.TOP_RIGHT].push($("#rightSearch"));
 
     lBox.addListener("places_changed", handleSearch.bind(lBox, lMap));
     rBox.addListener("places_changed", handleSearch.bind(rBox, rMap));
@@ -52,6 +56,10 @@ function handleRightZoom() {
 function handleSearch(map) {
     let places = this.getPlaces();
     map.setCenter(places[0].geometry.location);
+}
+
+function handleMapType() {
+    rMap.setMapTypeId(lMap.getMapTypeId());
 }
 
 window.$ = s => document.querySelector(s);
